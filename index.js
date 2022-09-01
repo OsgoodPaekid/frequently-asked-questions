5c6ceecd90cd8f7f67d1232dd964c91533c460a7
Osgood <osgoodackom@gmail.com>
2022-09-01 23:36:29 +0000

    added a little css

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const btns = document.querySelectorAll(".btn")
 const answerSection = document.querySelector(".answer-section")
+const questionItself = document.querySelectorAll(".question-itself")
 
 btns.forEach(function(btn){
     btn.addEventListener("click", function(e){
